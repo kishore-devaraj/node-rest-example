ec38f019caa5b0454a23f80e10981185ae4d2282
kishore-devaraj <kishoregrylls@gmail.com>
2018-08-13 03:33:53 +0530

    Typo fixed when declaring db instance

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-// MANGO_DB_URI = 'mongodb://admin:admin987@ds119572.mlab.com:19572/todos-app'
+// MANGO_DB_URI = 'mongodb://admin:admin@987@ds119572.mlab.com:19572/todos-app'
 
 // Connect to the mongodb using mongoose ORM
-mongoose.connect(process.env.PROD_MONGODB,{useNewUrlParser: true})
+mongoose.connect(process.env.PROD_MONGODB || 'mongodb://localhost:27017/todos-app')
 
 module.exports = {
   mongoose
