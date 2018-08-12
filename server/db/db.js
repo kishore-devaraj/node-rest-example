67e5276f084877003d256a9df4bf5bfec3d7a692
kishore-devaraj <kishoregrylls@gmail.com>
2018-08-13 03:18:53 +0530

    Added MONGO_DB_URI

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
+// MANGO_DB_URI = 'mongodb://admin:admin987@ds119572.mlab.com:19572/todos-app'
 
 // Connect to the mongodb using mongoose ORM
-mongoose.connect('mongodb://localhost:27017/TodosApp',{useNewUrlParser: true})
+mongoose.connect(process.env.PROD_MONGODB,{useNewUrlParser: true})
 
 module.exports = {
   mongoose
