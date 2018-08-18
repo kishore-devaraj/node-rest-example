824eafa72012bc909a709e48afdb48432a91634f
kishore-devaraj <kishoregrylls@gmail.com>
2018-08-18 19:09:46 +0530

    Added Unit test for DELETE /users/token/me

--- tests/server.test.js
+++ tests/server.test.js
@@ -298,3 +298,22 @@ describe('POST /users/login', () => {
   })
 })
 
+
+describe('DELETE /users/me/token' , () => {
+  it('should delete token when user signout', (done) => {
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', seedUsers[0].tokens[0].token)
+    .expect(200)
+    .end((err, res) => {
+      if (err) return done(err)
+
+      User.findById(seedUsers[0]._id)
+      .then( user => {  
+        expect(user.tokens.length).toBe(0)
+        done()
+      }).catch(err => done(err))
+    })
+  })
+})
+
